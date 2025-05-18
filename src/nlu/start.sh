30d0c08cb0e39fa7aab0d57a5b3ff811a5077c03
Fabiano França <email@fabianofranca.com>
2025-05-17 23:39:08 -0300

    Adiciona arquivos de configuração e scripts para o NLU, incluindo Dockerfile, fly.toml e start.sh, além de um modelo NLU pré-treinado.

--- src/nlu/start.sh
+++ src/nlu/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Garante permissão de execução
-chmod +x start.sh
-
-# mkdir -p models-empty
-
 # Inicia apenas o servidor NLU
-rasa run --enable-api --port 10000
+rasa run --enable-api --port 5005
